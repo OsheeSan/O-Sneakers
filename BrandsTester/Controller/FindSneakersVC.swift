77e8b1e3a492bc17b36a0b0420b904ad6e6812af
Anton Babko <anton.babko@ukma.edu.ua>
2023-02-15 07:25:46 +0200

    all sneakers search view

--- BrandsTester/Controller/FindSneakersVC.swift
+++ BrandsTester/Controller/FindSneakersVC.swift
@@ -1,32 +1,27 @@
 //
-//  BrandSneakersVC.swift
+//  FindSneakersVC.swift
 //  BrandsTester
 //
-//  Created by admin on 12.02.2023.
+//  Created by admin on 15.02.2023.
 //
 
 import UIKit
 
-class BrandSneakersVC: UIViewController {
-
-    var brand = "Error"
+class FindSneakersVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data = Data()
     var sneakers = [Sneakers]()
     var filteredSneakers = [Sneakers]()
     var choosenSneakers: Sneakers?
-    let data = Data()
 
-    @IBOutlet weak var tableView: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.keyboardDismissMode = .onDrag
-        view.backgroundColor = .black
-        overrideUserInterfaceStyle = .dark
-        title = brand
-        for i in 0..<data.brands.count{
-            if brand == data.brands[i].name {
-                sneakers = data.brands[i].goods
-                break
+        for brand in data.brands {
+            for sneaker in brand.goods {
+                sneakers.append(sneaker)
             }
         }
         filteredSneakers = sneakers
@@ -38,18 +33,18 @@ class BrandSneakersVC: UIViewController {
             controler.sneakers = choosenSneakers
         }
     }
-
+    
 }
 
-extension BrandSneakersVC: UITableViewDelegate, UITableViewDataSource{
+extension FindSneakersVC: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        filteredSneakers.count
+        return filteredSneakers.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "modelCell")
-        let modelLabel = cell?.viewWithTag(1) as! UILabel
-        modelLabel.text = filteredSneakers[indexPath.row].model
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sneakersCell")
+        let modelName = cell?.viewWithTag(1) as! UILabel
+        modelName.text = "\(filteredSneakers[indexPath.row].brand) \(filteredSneakers[indexPath.row].model)"
         return cell!
     }
     
@@ -62,18 +57,29 @@ extension BrandSneakersVC: UITableViewDelegate, UITableViewDataSource{
     
 }
 
-extension BrandSneakersVC: UISearchBarDelegate{
+extension FindSneakersVC: UISearchBarDelegate{
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchText.isEmpty {
             filteredSneakers = sneakers
         } else {
             filteredSneakers = sneakers.filter {
-                $0.model.lowercased().contains(searchText.lowercased())
+                if $0.model.lowercased().contains(searchText.lowercased())
                 || $0.id!.lowercased().contains(searchText.lowercased())
                 || $0.description!.lowercased().contains(searchText.lowercased())
+                    || $0.brand.localizedCaseInsensitiveContains(searchText) {
+                    return true
+                }
+                var sneakersName = $0.brand.lowercased() + " " + $0.model.lowercased()
+                if sneakersName.contains(searchText.lowercased()) {
+                    return true
+                }
+                sneakersName = $0.model.lowercased() + " " + $0.brand.lowercased()
+                if sneakersName.contains(searchText.lowercased()) {
+                    return true
+                }
+                return false
             }
         }
         tableView.reloadData()
     }
 }
-
